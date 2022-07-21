8aa25434ee218b6230c388436bb586fa3f4b1698
Gaëtan H <steellgold0@gmail.com>
2022-07-21 17:07:12 +0200

    Fichier principal (changement mineurs)

--- src/main/java/fr/steellgold/epicquests/Guild.java
+++ src/main/java/fr/steellgold/epicquests/Guild.java
@@ -1,13 +1,32 @@
 package fr.steellgold.epicquests;
 
+import fr.steellgold.epicquests.commands.GuildCommand;
+import fr.steellgold.epicquests.database.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Guild extends JavaPlugin {
+
+    public static DatabaseManager manager;
+
+    public static Guild instance;
+
     public void onEnable() {
         getLogger().info("Guild is enabled");
+        manager = new DatabaseManager();
+        instance = this;
+
+        getCommand("guild").setExecutor(new GuildCommand());
     }
 
     public void onDisable() {
         getLogger().info("Guild is disabled");
+    }
+
+    public static DatabaseManager getDatabaseManager() {
+        return manager;
+    }
+
+    public static Guild getInstance() {
+        return instance;
     }
 }
