a25d1a6623dbbe168bddc606cde959e22e94e571
Gaëtan H <steellgold0@gmail.com>
2022-07-21 17:07:44 +0200

    Fichier de l'instance des guildes (Introduction)

---
+++ src/main/java/fr/steellgold/epicquests/session/Guild.java
@@ -0,0 +1,13 @@
+package fr.steellgold.epicquests.session;
+
+import org.bukkit.entity.Player;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Guild {
+
+    public Guild(ResultSet rs) throws SQLException {
+
+    }
+}
